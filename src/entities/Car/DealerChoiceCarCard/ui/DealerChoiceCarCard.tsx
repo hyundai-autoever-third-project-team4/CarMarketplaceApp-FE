43d0c0105ef2b6d27c9fe7d1f09f56bc34477903
OhJeongHwan1 <wjdghks0316@naver.com>
2025-01-13 17:14:32 +0900

    feat: home 완성

--- src/entities/Car/DealerChoiceCarCard/ui/DealerChoiceCarCard.tsx
+++ src/entities/Car/DealerChoiceCarCard/ui/DealerChoiceCarCard.tsx
@@ -3,6 +3,8 @@ import { DealerChoiceCarCardProps } from "../model/type";
 import * as S from "./DealerChoiceCarCard.style";
 import pick1 from "@shared/assets/picking1.svg";
 import pick2 from "@shared/assets/picking2.svg";
+import morePick from "@shared/assets/morePick.svg";
+import { useNavigate } from "react-router-dom";
 
 export function DealerChoiceCarCard({
   id,
@@ -13,13 +15,31 @@ export function DealerChoiceCarCard({
   price,
   cardType,
 }: DealerChoiceCarCardProps) {
+  const navigate = useNavigate();
+
+  const moveToDetail = () => {
+    if (cardType === "main") navigate(`/carDetail/${id}`);
+  };
+
   return (
-    <S.Container $cardType={cardType}>
+    <S.Container $cardType={cardType} onClick={moveToDetail}>
       <S.CarImg src={mainImage} />
       {cardType === "main" && (
         <>
           <S.Picking1 src={pick1} />
           <S.Picking2 src={pick2} />
+        </>
+      )}
+      {cardType === "more" && (
+        <>
+          <S.CarBack />
+          <S.MorePick src={morePick} />
+        </>
+      )}
+      {cardType === "less" && (
+        <>
+          <S.CarBack2 />
+          <S.LessPick src={morePick} />
         </>
       )}
       <S.TextBox>
