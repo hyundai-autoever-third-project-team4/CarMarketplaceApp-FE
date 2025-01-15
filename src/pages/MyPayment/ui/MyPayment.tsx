e9a6ad0af0e36394d023c27b4d6a5e62aecbf049
yeon-dong <jae073@naver.com>
2025-01-15 10:48:44 +0900

    내가 구매한 차량 UI 개발 완료

--- src/pages/MyPayment/ui/MyPayment.tsx
+++ src/pages/MyPayment/ui/MyPayment.tsx
@@ -1,3 +1,48 @@
+import { Text } from "@/shared/ui/Text";
+import * as S from "./MyPayment.style";
+import NoCarIcon from "@/shared/assets/no_car.svg";
+import { MyPaymentCarCard } from "@/widgets/MyPaymentCarCard";
+import { mockPayCarData } from "@/widgets/MyPaymentCarCard/api/MockData";
+import { StatusType } from "@/widgets/MyPaymentCarCard/model/type";
+
 export function MyPayment() {
-  return <div>MyPayment</div>;
+  return (
+    <>
+      <S.Container>
+        <S.Title>
+          <Text fontType="title">내가 판매한 차량</Text>
+        </S.Title>
+        {mockPayCarData ? (
+          mockPayCarData.map((car) => (
+            <div key={car.id}>
+              <MyPaymentCarCard
+                name={car.name}
+                registrationDate={car.registrationDate}
+                mileage={car.mileage}
+                licensePlate={car.licensePlate}
+                price={car.price!}
+                mainImage={car.mainImage!}
+                isReviewed={car.isReviewed}
+                state={car.state as StatusType}
+              />
+              {car.isReviewed ? <S.MarginBottom /> : <S.ReviewMarginBottom />}
+            </div>
+          ))
+        ) : (
+          <S.NoDataContainer>
+            <S.NoDataImg src={NoCarIcon} />
+            <Text fontType="subTitle">아직 구매한 차량이 없습니다.</Text>
+            <S.NoDataSubText>
+              <Text fontType="sub1" fontColor="primary">
+                차자바에서 인증된
+              </Text>
+              <Text fontType="sub1" fontColor="primary">
+                나만의 중고차를 찾아보세요!
+              </Text>
+            </S.NoDataSubText>
+          </S.NoDataContainer>
+        )}
+      </S.Container>
+    </>
+  );
 }
---
+++ src/widgets/MyPaymentCarCard/api/MockData.ts
@@ -0,0 +1,62 @@
+export const mockPayCarData = [
+  {
+    id: "1",
+    mainImage:
+      "https://certified-static.hyundai.com/contents/goods/shootConts/tobepic/02/exterior/HIG241028009973/PRD602_233.JPG/dims/crop/2304x1536+600+770/resize/380x253/optimize",
+    name: "2023 GV70 가솔린 2.5 터보 AWD 스탠다드 디자인",
+    registrationDate: "2022-05-01",
+    mileage: 1500,
+    licensePlate: "12가 3456",
+    price: 1000,
+    isReviewed: true,
+    state: "Pending purchase approval", // 추가된 속성
+  },
+  {
+    id: "2",
+    mainImage:
+      "https://certified-static.hyundai.com/contents/goods/shootConts/tobepic/02/exterior/HIG241028009973/PRD602_233.JPG/dims/crop/2304x1536+600+770/resize/380x253/optimize",
+    name: "2022 현대 아반떼 1.6 가솔린",
+    registrationDate: "2022-03-15",
+    mileage: 8000,
+    licensePlate: "34나 5678",
+    price: 1500,
+    isReviewed: false,
+    state: "Not available for purchase", // 추가된 속성
+  },
+  {
+    id: "3",
+    mainImage:
+      "https://certified-static.hyundai.com/contents/goods/shootConts/tobepic/02/exterior/HIG241028009973/PRD602_233.JPG/dims/crop/2304x1536+600+770/resize/380x253/optimize",
+    name: "2021 기아 K5 2.0 가솔린",
+    registrationDate: "2021-08-20",
+    mileage: 12000,
+    licensePlate: "56다 7890",
+    price: 1800,
+    isReviewed: false,
+    state: "Pending purchase approval", // 추가된 속성
+  },
+  {
+    id: "4",
+    mainImage:
+      "https://certified-static.hyundai.com/contents/goods/shootConts/tobepic/02/exterior/HIG241028009973/PRD602_233.JPG/dims/crop/2304x1536+600+770/resize/380x253/optimize",
+    name: "2020 BMW 320i M 스포츠 라인",
+    registrationDate: "2020-11-10",
+    mileage: 25000,
+    licensePlate: "78라 9012",
+    price: 3000,
+    isReviewed: true,
+    state: "Not available for purchase", // 추가된 속성
+  },
+  {
+    id: "5",
+    mainImage:
+      "https://certified-static.hyundai.com/contents/goods/shootConts/tobepic/02/exterior/HIG241028009973/PRD602_233.JPG/dims/crop/2304x1536+600+770/resize/380x253/optimize",
+    name: "2023 테슬라 모델 3 롱 레인지",
+    registrationDate: "2023-01-25",
+    mileage: 500,
+    licensePlate: "90마 3456",
+    price: 6500,
+    isReviewed: false,
+    state: "Pending purchase approval", // 추가된 속성
+  },
+];
