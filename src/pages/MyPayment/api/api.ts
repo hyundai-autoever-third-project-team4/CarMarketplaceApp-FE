5d0b502c29c072156fbf68bf76f337296a14fd91
yeon-dong <jae073@naver.com>
2025-01-19 18:14:21 +0900

    내가 구매한 차량 api 연동 완료

---
+++ src/pages/MyPayment/api/api.ts
@@ -0,0 +1,32 @@
+import { authInstance } from "@/shared/api/axiosInstance";
+import { ResponseBody } from "@/shared/api/type";
+
+// 차량 정보 인터페이스
+export interface CarBuy {
+  carName: string;
+  licensePlate: string;
+  registrationDate: string;
+  mileage: number;
+  price: number;
+  mainImage: string;
+  status: string;
+  isReviewed: boolean;
+}
+
+// 판매 차량 목록 인터페이스
+export interface BuyCarListResponse {
+  userPurchaseCarList: CarBuy[];
+}
+
+// GET 요청을 보내는 함수
+export const handleBuyCarList = async () => {
+  try {
+    const response: ResponseBody<BuyCarListResponse> = await authInstance.get(
+      `/mypage/purchase`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Car purchase fetch failed:", error);
+    throw error;
+  }
+};
