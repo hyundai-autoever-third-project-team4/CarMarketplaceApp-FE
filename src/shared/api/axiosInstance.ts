71dc9d353dcd9fa57d39b7e5f9741df2a2af13cf
yeon-dong <jae073@naver.com>
2025-01-20 11:25:27 +0900

    로그인 새로고침 이슈 수정 중

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -2,17 +2,20 @@ import axios, { AxiosInstance } from "axios";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-const getToken = () => {
-  console.log("겟토큰 메서드 호출:", localStorage.getItem("access_token"));
-  return localStorage.getItem("access_token");
-};
-
 export const authInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${getToken()}`, // access token 추가
   },
+});
+
+// 요청 인터셉터 추가
+authInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export const noAuthInstance: AxiosInstance = axios.create({
