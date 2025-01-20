a06f05eca3dd8964c24abba9934c2d48a8d9b58d
OhJeongHwan1 <wjdghks0316@naver.com>
2025-01-20 17:20:50 +0900

    feat: 알림 연결

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,9 +4,28 @@ import { Header } from "@/widgets/Header";
 import * as S from "./App.style";
 import { BottomNavigation } from "@/widgets/BottomNavigation";
 import { useScrollToTop } from "@/shared/hooks/useScrollTop";
+import { useEffect } from "react";
+import useLoginCheckStore from "@/shared/store/loginStore";
+
+declare global {
+  interface Window {
+    Android2?: {
+      getToken: (userId: number) => void;
+    };
+  }
+}
 
 function App() {
   useScrollToTop();
+  const { loginCheck } = useLoginCheckStore();
+
+  useEffect(() => {
+    const userId: string | null = localStorage.getItem("userId");
+    console.log(userId);
+    if (userId !== null && window.Android2) {
+      window.Android2.getToken(Number(userId));
+    }
+  }, [loginCheck]);
 
   return (
     <>
