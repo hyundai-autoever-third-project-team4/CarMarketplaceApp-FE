cf39bacf9cf3da5e77e65c5d2d04b4dd2585a136
OhJeongHwan1 <wjdghks0316@naver.com>
2025-01-20 15:19:30 +0900

    feat: popular 차량 불러오기

--- src/entities/Car/PopularCarCard/api/api.ts
+++ src/entities/Car/PopularCarCard/api/api.ts
@@ -1,11 +1,11 @@
-import POPULAR_CARS from "./mock";
 import { PopularCar } from "../model/type";
+import { noAuthInstance } from "@/shared/api/axiosInstance";
+import { ResponseBody } from "@/shared/api/type";
 
 export const getPopularCars = async (): Promise<PopularCar[]> => {
-  // 실제 API 호출 대신 Promise를 사용해 mock 데이터를 반환
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(POPULAR_CARS);
-    }, 20); // 500ms 딜레이 추가
-  });
+  const result: ResponseBody<Promise<PopularCar[]>> = await noAuthInstance.get(
+    "/cars/top-liked"
+  );
+
+  return result.data;
 };
